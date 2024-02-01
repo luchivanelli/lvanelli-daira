d540c8b5f9e55645a0b0f1c35f56b82497dca69f
Luciana <lucianavanelli13gmail.com>
2024-02-01 17:15:28 -0300

    1 hora +Funcionamiento de operaciones +Visualizacion del resultado en display -Poder realizar operaciones de mas de 2 numeros a la vez

--- calculadora/src/components/Calculadora.svelte
+++ calculadora/src/components/Calculadora.svelte
@@ -3,28 +3,49 @@
 
     const buttons = ['C', '/', '*', '-', 7, 8, 9, '+', 4, 5, 6, 1, 2, 3, '=', 0, '.']
     let valueDiv = '0'
-    let a, b
 
     const handleDiv = (e)=> {
         let value = e.target.innerHTML
-        if (valueDiv == '0') {
+        if (valueDiv == '0') {  //limpiar el '0' inicial
             valueDiv = ''
             valueDiv = valueDiv + value
         } else {
             valueDiv = valueDiv + value
         }
 
-        if (value == 'C') {
+        if (value == 'C') {  //limpiar display
             valueDiv = '0'
-        }
-
-        if (value == '=') {
+        } else if (value == '=') {  //operar
             operation()
         }
-        
     }
 
     const operation = ()=> {
+        let newValue, resultado
+        if (valueDiv.includes('+')) {  //verificar el operador
+            newValue = valueDiv.split('+')  //separar string, obtener numeros a operar
+            newValue[1] = newValue[1].replace('=', '')  //eliminar '=' del 2do numero
+            resultado = parseFloat(newValue[0]) + parseFloat(newValue[1])  //realizar operacion
+
+        } else if (valueDiv.includes('-')) {
+            newValue = valueDiv.split('-')
+            newValue[1] = newValue[1].replace('=', '')
+            resultado = parseFloat(newValue[0]) - parseFloat(newValue[1])
+
+        } else if (valueDiv.includes('*')) {
+            newValue = valueDiv.split('*')
+            newValue[1] = newValue[1].replace('=', '')
+            resultado = parseFloat(newValue[0]) * parseFloat(newValue[1])
+
+        } else if (valueDiv.includes('/')) {
+            newValue = valueDiv.split('/')
+            newValue[1] = newValue[1].replace('=', '')
+            resultado = parseFloat(newValue[0]) / parseFloat(newValue[1])
+
+        }
+    
+        resultado % 1 == 0 ? valueDiv = resultado.toString() : valueDiv = resultado.toFixed(3).toString()
+        //si el numero es flotante, limitar la cantidad de decimales. Pasar variable de number a string
     }
 
 </script>
