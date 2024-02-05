6b6ff63f490cc0aebf7ac2949ca011d5a9fbac51
Luciana <lucianavanelli13gmail.com>
2024-02-05 15:00:15 -0300

    instalacion cors

--- calculadora/Backend/main.go
+++ calculadora/Backend/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 )
 
 type operation struct {
@@ -14,7 +14,7 @@ type operation struct {
 }
 
 var operations = []operation{
-	{Operation_ID: 1, Operation: "10-3", Result: "7"}
+	{Operation_ID: 1, Operation: "10-3", Result: "7"},
 }
 
 func getData(c *gin.Context) {
@@ -32,25 +32,27 @@ func postData(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, operations)
 }
 
-func getOperationID(c *gin.Context) {
-	id := c.Param("id")
 
-	for _, a := range albums {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
-			return
-		}
-	}
+// func getOperationID(c *gin.Context) {
+// 	id := c.Param("id")
 
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Operation not found"})
-}
+// 	for _, a := range operations {
+// 		if a.ID == id {
+// 			c.IndentedJSON(http.StatusOK, a)
+// 			return
+// 		}
+// 	}
+
+// 	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Operation not found"})
+// }
 
 func main() {
 	fmt.Println(operations)
 	router := gin.Default()
+	router.Use(cors.Default())
 	router.GET("/operations", getData)
 	router.POST("/operations", postData)
-	router.GET("/operations/:id", getOperationID)
+	// router.GET("/operations/:id", getOperationID)
 
 	router.Run("localhost:5173")
 }
